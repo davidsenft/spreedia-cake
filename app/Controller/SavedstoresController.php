a32c0437c7d44e699162572c80e86f753eabe36a
David Senft <dave@thisoneisme.com>
2013-04-10 19:45:35 -0400

    Testing out a getStorenamesByUserId action… probably won't use this

--- app/Controller/SavedstoresController.php
+++ app/Controller/SavedstoresController.php
@@ -10,6 +10,9 @@ class SavedstoresController extends AppController {
 	public $name = 'Savedstores';
 	public $scaffold;
 
+	// var $helpers = array('Cache');
+	// var $cacheAction = "10 seconds";
+
 	public function add(){ /* $user_id, $storename_id */
 		$this->Savedstore->create();
 		$this->Savedstore->save($this->request->data);
@@ -21,5 +24,40 @@ class SavedstoresController extends AppController {
 		$this->Savedstore->delete($id);
 		$this->respond($id['id']);
 	}
+
+	// TODO: this can just be "view"? or is this actually better as its own thing?
+	/* public function getStorenamesByUserId($userid){
+
+		// get the user
+		// TODO: probably better to put this in the User controller
+		$User = ClassRegistry::init('User');
+		$usr = $User->findById($userid);
+
+		// get saved stores by user id
+		// TODO: faster to use findById with ids from User now that we have them?
+		$this->Savedstore->recursive = 2; // superfluous right now
+		$this->Savedstore->unbindModel(array('belongsTo' => array('User')));
+		$savedstores = $this->Savedstore->findAllByUserId($userid);
+		
+		// trim to savedstore_id => storename
+		$trimsavedstores = array();
+		foreach($savedstores as $ss){
+			$trimsavedstores[$ss['Savedstore']['id']] = $ss['Storename'];
+		}
+
+		// page data
+		$page = array(
+			'type' => "manystores", // as opposed to a single "store" TODO: not using this yet... remove? or probably make it "location"
+			'title' => $usr['User']['username'] . "'s favorite stores",
+			'seotitle' => $usr['User']['username'] . "'s favorite stores | Spreedia",
+			'format' => 'TODO'
+		);
+
+		// $this->respond($trimsavedstores);
+
+		$this->set('stores', $trimsavedstores);
+		$this->set('page', $page);
+		$this->set('_serialize', array('stores', 'page'));
+	} */
 
 }
--- app/Model/Savedstore.php
+++ app/Model/Savedstore.php
@@ -8,8 +8,10 @@ App::uses('AppModel', 'Model');
 class Savedstore extends AppModel {
 
 	public $name = "Savedstore";
+	public $actsAs = array('Containable');
+	public $recursive = 2;
 
-	var $cacheQueries = false;
+	// var $cacheQueries = false;
 
 	public $belongsTo = array('Storename', 'User');
 
