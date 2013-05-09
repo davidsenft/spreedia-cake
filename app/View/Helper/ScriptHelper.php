ca514c76e78009b9b189a85476438908da20a27e
David Senft <dave@thisoneisme.com>
2013-05-08 23:50:09 -0400

    Removed unnecessary php webroot calls

--- app/View/Helper/ScriptHelper.php
+++ app/View/Helper/ScriptHelper.php
@@ -18,17 +18,18 @@ class ScriptHelper extends AppHelper {
 		?>
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script type="text/javascript">
-	window.jQuery || document.write('<script src="<?php echo $this->webroot; ?>js/jquery-1.9.1.js"><\/script>')</script>
-<script type="text/javascript" src="<?php echo $this->webroot; ?>js/jquery-ui-1.10.1.custom.min.js"></script>
-<script type="text/javascript" src="<?php echo $this->webroot; ?>js/handlebars-1.0.rc.1.js"></script>
-<script type="text/javascript" src="<?php echo $this->webroot; ?>js/handlebars-helpers.js"></script>
-<script type="text/javascript" src="<?php echo $this->webroot; ?>js/spreedia.js"></script><?php 
+	window.jQuery || document.write('<script src="/js/jquery-1.9.1.js"><\/script>')</script>
+<script type="text/javascript" src="/js/jquery-ui-1.10.1.custom.min.js"></script>
+<script type="text/javascript" src="/js/jquery.iosslider.js"></script>
+<script type="text/javascript" src="/js/handlebars-1.0.rc.1.js"></script>
+<script type="text/javascript" src="/js/handlebars-helpers.js"></script>
+<script type="text/javascript" src="/js/spreedia.js"></script><?php 
 
 		/* if ($format == "map"){ TODO: load this asynchronously in changeFormat() as needed? */
 
 		?>
 <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDKlAkDEo8E6j9NI1xru5cRCSHqCxzy2kM&sensor=false"></script>
-<script type="text/javascript" src="<?php echo $this->webroot; ?>js/spreedia-map.js"></script><?php 
+<script type="text/javascript" src="/js/spreedia-map.js"></script><?php 
 
 		/*} */
 
